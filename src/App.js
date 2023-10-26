4694f6071e6a7fa261d143b550a1474061799f57
Anjali Kumari <anjaliagrawal23011999@gmail.com>
2023-10-26 11:23:32 +0530

    Implemented loading and error state

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import { convertToDateFormat } from "./utils/utils";
 import "./App.css";
 
 function App() {
-  const { items, hasNextPage, handleCallback } = useNodes();
+  const { isLoading, isError, error, items, hasNextPage, handleCallback } =
+    useNodes();
 
   useEffect(() => {
     const handleScroll = () => {
@@ -24,20 +25,23 @@ function App() {
 
   return (
     <div>
-      {items.map((data, key) => (
-        <div className="image-gallery" key={key}>
-          <img
-            className="image-field"
-            src={`${data.node.field_photo_image_section}`}
-          />
-          <div className="data-field">
-            <h3 className="title">{data.node.title}</h3>
-            <h4 className="last-update">
-              {convertToDateFormat(data.node.last_update)}
-            </h4>
+      {isLoading && <h4>Please Wait for a moment!!</h4>}
+      {isError && <h3 style={{ color: "red" }}>{error?.message}</h3>}
+      {!isError &&
+        items?.map((data, key) => (
+          <div className="image-gallery" key={key}>
+            <img
+              className="image-field"
+              src={`${data.node.field_photo_image_section}`}
+            />
+            <div className="data-field">
+              <h3 className="title">{data.node.title}</h3>
+              <h4 className="last-update">
+                {convertToDateFormat(data.node.last_update)}
+              </h4>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
     </div>
   );
 }
